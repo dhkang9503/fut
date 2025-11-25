9f8ce50a0b8629a2e0da55590e4d723246da2743
DonghyunKang <88758902+dhkang9503@users.noreply.github.com>
2025-11-25 20:06:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -191,11 +191,11 @@ def detect_cci_signal(df):
     side = None
     stop_price = None
 
-    if cci_prev > 100 and 70 <= cci_curr <= 99:   # 숏 진입
+    if (cci_prev > 100) and (70 <= cci_curr <= 99):   # 숏 진입
         side = "short"
         stop_price = float(curr["high"]) * 1.01
 
-    elif cci_prev < -100 and -70 >= cci_curr >= -99:  # 롱 진입
+    elif (cci_prev < -100) and (-70 >= cci_curr >= -99):  # 롱 진입
         side = "long"
         stop_price = float(curr["low"]) * 0.99
 
