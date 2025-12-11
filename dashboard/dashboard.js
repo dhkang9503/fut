d1c11269557c055ff40d4dd310fd89505cc52f83
DonghyunKang <88758902+dhkang9503@users.noreply.github.com>
2025-12-11 12:28:03 +0900

    Update dashboard.js

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -2,16 +2,16 @@
 // 기본 설정
 // =====================
 
-const SYMBOLS = ["ETH/USDT:USDT", "OKB/USDT:USDT", "SOL/USDT:USDT"];
+const SYMBOLS = ["AVAX/USDT:USDT", "OKB/USDT:USDT", "SOL/USDT:USDT"];
 
 const CHART_IDS = {
-    "ETH/USDT:USDT": "chart-btc",
+    "AVAX/USDT:USDT": "chart-btc",
     "OKB/USDT:USDT": "chart-eth",
     "SOL/USDT:USDT": "chat-sol",
 };
 
 const CCI_CHART_IDS = {
-    "ETH/USDT:USDT": "cci-btc",
+    "AVAX/USDT:USDT": "cci-btc",
     "OKB/USDT:USDT": "cci-eth",
     "SOL/USDT:USDT": "cci-sol",
 };
